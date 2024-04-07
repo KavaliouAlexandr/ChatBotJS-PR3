06856d9669d55caa0b78a9da20c8143e0a1b64ec
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2024-04-07 20:29:50 +0200

    chatbot response

--- src/script.js
+++ src/script.js
@@ -1,12 +1,28 @@
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
 
 
 let userMessage;
 
+const createChatLi = (message, className) => {
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outcome" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</pan><p>${message}</p>`;
+    chatLi.innerHTML = chatContent;
+    return chatLi;
+}
+
 const handleChat = () => {
     userMessage = chatInput.value.trim();
     console.log(userMessage);
+    if(!userMessage) return;
+
+    chatbox.appendChild(createChatLi(userMessage, "outcome"));
+
+    setTimeout(() => {
+        chatbox.appendChild(createChatLi("Typing..", "incoming"));
+    },600);
 }
 sendChatBtn.addEventListener("click", handleChat);
 
